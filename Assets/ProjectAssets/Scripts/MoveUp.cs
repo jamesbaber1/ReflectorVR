11f34133784de9d128bdf163cce9d1ff335924e6
privateryan1111 <ryanpfeifer1111@gmail.com>
2019-04-04 10:53:00 -0500

    added prelimiary scripts for enemy hierarchy

---
+++ Assets/ProjectAssets/Scripts/MoveUp.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUp : MonoBehaviour
+{
+
+    public float moveUpVal = 0.1f;
+    public float maxVal = 4;
+    public float minVal = -1;
+    public bool movingUp = true;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(gameObject.transform.position.y);
+        if(gameObject.transform.position.y >= maxVal)
+        {
+            movingUp = false;
+        }
+        else if (gameObject.transform.position.y <= minVal)
+        {
+            movingUp = true;
+        }
+
+        if (movingUp)
+        {
+            rb.velocity = new Vector3(0, moveUpVal, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector3(0, /*-moveUpVal*/0, 0);
+        }
+
+    }
+}
