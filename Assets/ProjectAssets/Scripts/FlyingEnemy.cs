7387b1a57411c88e8bbff6963fc188efec577b10
Pfeifer <privateryan1111@tamu.edu>
2019-04-08 20:36:09 -0500

    added preliminary flying enemies

--- Assets/ProjectAssets/Scripts/FlyingEnemy.cs
+++ Assets/ProjectAssets/Scripts/FlyingEnemy.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FlyingEnemy : MovingEnemy
 {
+    private NavMeshAgent agent;
+    private int frameNum = 0;
+    public float radius;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        radius = Random.Range(5.0f, 20.0f);
+        SpawnLazer();
+        Vector3 goTo = new Vector3(radius * Mathf.Cos(Random.Range(-2 * Mathf.PI, 2 * Mathf.PI)), transform.position.y, radius * Mathf.Sin(Random.Range(-2 * Mathf.PI, 2 * Mathf.PI)));
+        agent.SetDestination(goTo);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.LookAt(player.transform);
+        frameNum++;
+        if (frameNum % 300 == 0)
+        {
+            frameNum = 0;
+            Vector3 goTo = new Vector3(radius * Mathf.Cos(Random.Range(-2 * Mathf.PI, 2 * Mathf.PI)), 0, radius * Mathf.Sin(Random.Range(-2 * Mathf.PI, 2 * Mathf.PI)));
+            agent.SetDestination(goTo);
+        }
     }
 }
---
+++ Assets/ProjectAssets/Scripts/FlyingManager.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingManager : MonoBehaviour
+{
+    public GameObject turretPrefab;
+    public float radius;
+    public float numberOfTurrets;
+    public int maxTurrets;
+    public float activationCooldown;
+    public float decreaseLazerCooldown;
+    public float increaseLazerSpeed;
+
+    private int turretIterator = 0;
+    private float cooldownFrames = 300;
+    private List<GameObject> turrets;
+    private float frames = 0;
+    private bool findTurret = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        turrets = new List<GameObject>();
+        float angle = 0;
+        int iterations = 0;
+        while (iterations < numberOfTurrets)
+        {
+            Vector3 position = new Vector3(transform.position.x + radius * Mathf.Cos(Mathf.Deg2Rad * angle), transform.position.y, transform.position.z + radius * Mathf.Sin(Mathf.Deg2Rad * angle));
+            Quaternion rot = Quaternion.AngleAxis(-angle, Vector3.up);
+            GameObject o = Instantiate(turretPrefab, position, rot);
+            turrets.Add(o);
+            angle += (360 / numberOfTurrets);
+            iterations++;
+        }
+        SelectTurrets();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (findTurret == true)
+        {
+            if (turrets[turretIterator].GetComponent<Enemy>().getActive() == false)
+            {
+                activateTurret(turretIterator);
+                findTurret = false;
+            }
+            turretIterator++;
+            if (turretIterator >= numberOfTurrets)
+            {
+                turretIterator = 0;
+            }
+        }
+        else
+        {
+            frames++;
+            if (frames >= cooldownFrames)
+            {
+                if (turretIterator >= numberOfTurrets)
+                {
+                    turretIterator = 0;
+                }
+                if (turrets[turretIterator].GetComponent<Enemy>().getActive() == false)
+                {
+                    activateTurret(turretIterator);
+                    turretIterator++;
+                }
+                else
+                {
+                    findTurret = true;
+                }
+                frames = 0;
+                if (cooldownFrames > activationCooldown)
+                {
+                    cooldownFrames -= activationCooldown;
+                    for (int i = 0; i < numberOfTurrets; i++)
+                    {
+                        GameObject o = turrets[i];
+                        Enemy t = o.GetComponent<Enemy>();
+                        t.SetLazerCooldown(t.GetLazerCooldown() - /*0.1f*/decreaseLazerCooldown);
+                        t.SetLazerSpeed(t.GetLazerSpeed() + /*.1f*/increaseLazerSpeed);
+                    }
+                }
+
+            }
+        }
+    }
+
+    void SelectTurrets()
+    {
+        List<GameObject> turretOrder = new List<GameObject>();
+        for (int i = 0; i < numberOfTurrets; i++)
+        {
+            int rand = (int)(Random.value * turrets.Count);
+            turretOrder.Add(turrets[rand]);
+            turrets.RemoveAt(rand);
+        }
+
+        turrets.Clear();
+        turrets = turretOrder;
+    }
+
+    void activateTurret(int i)
+    {
+        GameObject o = turrets[i];
+        Enemy t = o.GetComponent<Enemy>();
+        t.setActive(true);
+    }
+
+    void deactivateTurret(int i)
+    {
+        GameObject o = turrets[i];
+        Enemy t = o.GetComponent<Enemy>();
+        t.setActive(false);
+    }
+}
