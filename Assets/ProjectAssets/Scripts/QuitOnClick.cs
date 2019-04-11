57dd2b3ca385c4445a3e82f2e5b3ce7d85fcd587
hiattm <hiattm@tamu.edu>
2019-04-11 10:37:11 -0500

    Switched to two scene system
    StartScene/MainScene

---
+++ Assets/ProjectAssets/Scripts/QuitOnClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    // Quit Game
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+          UnityEditor.EditorApplication.isPlaying = False;
+        #else
+          Application.Quit();
+        #endif
+    }
+
+}
