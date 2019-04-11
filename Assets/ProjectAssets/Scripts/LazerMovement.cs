a09c8d0a73059a4cd7c7ff40eee2f831ad2672a1
Pfeifer <privateryan1111@tamu.edu>
2019-04-11 11:17:37 -0500

    added assets

--- Assets/ProjectAssets/Scripts/LazerMovement.cs
+++ Assets/ProjectAssets/Scripts/LazerMovement.cs
@@ -19,7 +19,7 @@ public class LazerMovement : MonoBehaviour
     AudioSource hitShieldSound;
 
     // Start is called before the first frame update
-    void Start()
+    protected void Start()
     {
         target = GameObject.FindGameObjectWithTag("MainCamera");
         toPlayer = (target.transform.position - transform.position).normalized;
---
+++ Assets/ProjectAssets/Scripts/PistolScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using Valve.VR;
+
+namespace Valve.VR.InteractionSystem
+{
+    public class PistolScript : MonoBehaviour
+    {
+        public SteamVR_Action_Boolean trigger;
+        public GameObject lazer;
+        public SteamVR_Input_Sources triggerHand;
+        // Start is called before the first frame update
+        void Start()
+        {
+            //trigger.AddOnStateDownListener("firePistol", triggerHand);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                if (!GameObject.Find("PlayerLazer(Clone)")) Instantiate(lazer, transform.position + (transform.forward * 0.57f) + (transform.up * 0.06f), transform.rotation);
+            }
+        }
+
+        //void firePistol(OnPressDown, trigger)
+        //{
+
+        //}
+
+        
+    }
+}
