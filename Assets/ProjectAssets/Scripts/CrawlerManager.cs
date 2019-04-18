e68d34330a17a913ad7984def95b53a34fe63483
Pfeifer <privateryan1111@tamu.edu>
2019-04-18 11:37:21 -0500

    Maya made it beautiful

--- Assets/ProjectAssets/Scripts/CrawlerManager.cs
+++ Assets/ProjectAssets/Scripts/CrawlerManager.cs
@@ -21,7 +21,7 @@ public class CrawlerManager : MonoBehaviour
     private List<GameObject> turrets;
     private float frames = 0;
     private bool findTurret = false;
-    private int maxRounds = 5;
+    private int maxRounds = 2;
     private int round = 0;
     //private bool deactivated = false;
 
